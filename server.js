e7917b2081a1b9cdd978e5db9fe6b878a1610141
MADHAV BAHL <madhavbahl10@gmail.com>
2017-08-29 18:42:39 +0530

    GeoLocation API

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const yargs = require('yargs');
 const hbs = require('hbs');
 const fs =require('fs');
+
+const geocode = require('./geocode/geocode.js');
+const weather = require('./weather/weather.js');
 
 const port = process.env.PORT || 8080;
 
 var app = express();
+app.use(bodyParser());
 
 app.set('view engine','hbs');
 
@@ -12,6 +18,28 @@ app.use(express.static(__dirname + '/views'));
 
 app.get('/',(req,res) => {
   res.render('home.hbs');
+});
+
+app.post('/', function(req, res){
+  var location = req.body.location;
+
+  geocode.geocodeAddress(location,(err,results) => {
+    if(err) {
+      res.send(`ERROR: ${err}`);
+    } else {
+      var html = `<h1>LOCATION : ${results.address} </h1>`
+      weather.getWeather(results.latitude,results.longitude, (errorMessage,weatherResults) => {
+      if(errorMessage) {
+        app.send(errorMessage);
+      }  else {
+        app.send(`It's currently : ${weatherResults.temperature} deg F. It feels like ${weatherResults.apperentTemperature} deg F`);
+      }
+    });
+    }
+  });
+  // var html = 'Hello: ' + location + '.<br>' +
+  //            '<a href="/">Try again.</a>';
+  // res.send(html);
 });
 
 // app.get('/',(req,res) => {
