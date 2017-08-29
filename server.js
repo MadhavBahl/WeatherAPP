08898c74e8d004e1f756275fa0525c4e1a42527f
MADHAV BAHL <madhavbahl10@gmail.com>
2017-08-29 12:52:49 +0530

    Setting up the server

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs =require('fs');
+
+const port = process.env.PORT || 8080;
+
+var app = express();
+
+app.set('view engine','hbs');
+
+app.get('/',(req,res) => {
+  res.render('home.hbs');
+});
+
+// app.get('/',(req,res) => {
+//   res.send({
+//     title: 'Hello World',
+//     page: 'Home page',
+//     contents: {
+//       name: 'Madhav',
+//       lastName: 'Bahl',
+//       age: '19',
+//       skills: 'Web Developer'
+//     }
+//   });
+// });
+
+
+app.listen(port, () => {
+  console.log(`App is up on server ${port}`);
+});
