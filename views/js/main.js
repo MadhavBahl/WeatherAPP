669e51ae03043a8869cc27ba2ab33afdce0b1b66
MADHAV BAHL <madhavbahl10@gmail.com>
2017-08-30 18:20:29 +0530

    Doing the css

--- views/js/main.js
+++ views/js/main.js
@@ -1,7 +1,17 @@
 function showLoader() {
   var overlay = document.getElementById("overlay");
+  var mainContent = document.getElementById("mainContent");
   overlay.style.display = 'block';
+  mainContent.style.display = 'none';
   setTimeout( function(){
     overlay.style.display = 'none';
-  }, 7800);
+    mainContent.style.display = 'block';
+  }, 3800);
 }
+ /* ======= Search Bar Animation ======= */
+ // function ChangeColor() {
+ //   var color = document.getElementById('myColor').value;
+ //   document.getElementById('ColorCode').style.color = color;
+ //   document.getElementById('search-button').style.background = color;
+ //   document.getElementById("ColorCode").innerHTML = color;
+ // }
