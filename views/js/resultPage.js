52553fd8086e7cf90629c029a6bc718b69eb0116
MADHAV BAHL <madhavbahl10@gmail.com>
2017-09-01 23:30:08 +0530

    Conditions Done

--- views/js/resultPage.js
+++ views/js/resultPage.js
@@ -1,4 +1,4 @@
-var mainContent = document.getElementById9
+var mainContent = document.getElementById('mainContent');
 var lat = document.getElementById('lat').textContent;
 lat = Number(lat);
 var lng = document.getElementById('lng').textContent;
@@ -6,7 +6,49 @@ lng = Number(lng);
 console.log(lat);
 console.log(lng);
 
-// var currentIcon = document.getElementById('currentIcon').textContent;
+var currentIcon = document.getElementById('currentIcon').textContent;
+var currentWeather = document.getElementById('currentWeather');
+var lineBreak = document.getElementById('lineBreak');
+var currentData = document.getElementById('currentData');
+var showMore = document.getElementById('showMore');
+console.log(currentIcon);
+
+if(currentIcon === 'rain'){
+  mainContent.style.backgroundImage = "url('../img/rain.jpg')";
+  currentWeather.style.color = '#C7C7C7';
+  lineBreak.src = '../img/break2.png';
+  currentData.style.color = '#C0C0C0';
+} else if(currentIcon === 'clear-day'){
+  mainContent.style.backgroundImage = "url('../img/clear-day.jpg')";
+} else if(currentIcon === 'clear-night'){
+  mainContent.style.backgroundImage = "url('../img/clear-night.jpg')";
+  currentWeather.style.color = '#C7C7C7';
+  lineBreak.src = '../img/break2.png';
+  currentData.style.color = '#C0C0C0';
+} else if(currentIcon === 'partly-cloudy-night'){
+  mainContent.style.backgroundImage = "url('../img/partly-cloudy-night.jpg')";
+  currentWeather.style.color = '#C7C7C7';
+  lineBreak.src = '../img/break2.png';
+  currentData.style.color = '#C0C0C0';
+} else if(currentIcon === 'partly-cloudy-day'){
+  mainContent.style.backgroundImage = "url('../img/partly-cloudy-day.jpg')";
+} else if(currentIcon === 'cloudy'){
+  mainContent.style.backgroundImage = "url('../img/cloudy.jpg')";
+  currentWeather.style.color = '#C7C7C7';
+  lineBreak.src = '../img/break2.png';
+  currentData.style.color = '#C0C0C0';
+} else if(currentIcon === 'fog'){
+  mainContent.style.backgroundImage = "url('../img/fog.jpg')";
+} else if(currentIcon === 'sleet'){
+  mainContent.style.backgroundImage = "url('../img/sleet.jpg')";
+  currentWeather.style.color = '#C7C7C7';
+  lineBreak.src = '../img/break2.png';
+  currentData.style.color = '#C0C0C0';
+} else if(currentIcon === 'snow'){
+  mainContent.style.backgroundImage = "url('../img/snow.jpg')";
+} else if(currentIcon === 'wind'){
+  mainContent.style.backgroundImage = "url('../img/wind.jpg')";
+}
 
 
 function initMap() {
